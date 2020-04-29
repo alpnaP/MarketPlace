faa753f99fadd837a81fa6f0a8ef96ac031938e8
alpnaP <1703alpanapatel@gmail.com>
2020-04-30 02:04:26 +0530

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-constexpress = require("express");
-constapp = expres();
-constpath = require("path");
+const express = require("express");
+const app = expres();
+const path = require("path");
 app.us(express.static(__dirname + "/dist"));
-app.listen(process.env.PORT || 8080);
+
 //PathLocationStradegy
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
