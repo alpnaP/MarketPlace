21f1b1b5a7315fece7b869b5c60441d7233d3b72
anuram <anuram@wimerasys.com>
2021-05-23 00:28:33 +0530

    updated portfolio

--- src/app/component/gallery/gallery.component.ts
+++ src/app/component/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -10,6 +11,8 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+ 
 
 }
